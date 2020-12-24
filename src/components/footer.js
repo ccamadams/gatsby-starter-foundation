df045d03187a24ba132e4175de0d09f51ab52664
Cam Adams <camadams@charter.net>
2020-12-24 13:09:44 -0600

    changed foot and home page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { Link } from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+/* import { Link } from "gatsby"
+import { RiHeart2Line } from "react-icons/ri"; */
 
 const Footer = () => (
   <footer 
@@ -11,7 +11,7 @@ const Footer = () => (
     }}
   >
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      {/* <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p> */}
     </div>
   </footer>
 )
--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -1,11 +1,11 @@
 ---
 template: index-page
 slug: /
-title: Wyatt Morriss
-tagline: Graphic Designer
+title: Cam Adams
+tagline: Software Developer 
 featuredImage: /assets/toa-heftiba-0rlfirsdvzu-unsplash.jpg
 cta:
   ctaText: Know more
   ctaLink: /about
 ---
-He is a award winning gaming graphic designer, based in sunny side of California. Working as Principal designer at Bethesda
+Welcome! I am a software developer working on my masters at Auburn University. This is a place to learn about me
